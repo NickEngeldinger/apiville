c84b03f00da45d6859b0f87f9f13c26939b71c52
Nick Engeldinger <nick@nick-LT20.(none)>
2013-12-09 16:16:04 -0800

    Merged bootstrap template with html5 boilerplate

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
+var menuToggle = function() {
+	var 
+		$menu    = $('#menu-toggle'),
+		$wrapper = $('#wrapper');
 
+	$menu.on('click',function(e) {
+		e.preventDefault();
+		$wrapper.toggleClass('active');
+	});
+}
+menuToggle();

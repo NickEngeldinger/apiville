ad35776d9e736b7dea219944722bc13e3593b158
Nick Engeldinger <nick@nick-LT20.(none)>
2013-12-10 22:04:17 -0800

    Adding show/hide functionality to sports page

--- js/sports.js
+++ js/sports.js
@@ -26,22 +26,54 @@ $.ajax({
 	},
 	dataType : 'jsonp',
 	success  : function(data) {
-		var ul = $('.teams');
-		$.each(data.headlines, function() {
-			var li = $('<li/>').text(this.headline);
-			ul.append(li);
-			console.log('Headlines ->', this)
+		var 
+			$ul = $('.teams'),
+			btnEl = '<br /><button class="reveal">Reveal</button>';
 
-			$.each(this.links, function() {
-				
-				console.log('Links ->', this)
+		var revealMore = function() {
+			var 
+				$btn     = $('.reveal'),
+				closeBtn = '<button class="close">Close</button>';
+
+			$btn.on('click', function(event) {
+				var thisTarget = event.currentTarget;
+				console.log(thisTarget)
+				console.log(this)
+				$('.reveal + .story').show('4000','swing');
 			});
+		};
+
+		
+		$.each(data.headlines, function() {
+			var 
+				$li  = $('<li/ class="team_headline">'),
+				$div = $('<div class="story" />'),
+				$a   = $('<a target="blank"/>');
+
+			$li.text(this.headline);
+			$div.text(this.description);
+			$a.text('Read More').attr('href', this.links.web.href)
+
+			$ul.append($li);
+
+			$li.append(btnEl);
+			$li.append($div)
+
+			$div.append($a);
 
 		});
-		console.log(data);
+
+		revealMore();
+		
+		
+
 	},
 	error : function() {
 		li = '<li>Sorry, there was an error retrieving data from the server.</li>';
 		ul.append(li)
 	}
 });
+
+
+
+
